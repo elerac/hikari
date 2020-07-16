aa88d0b870d42beb2ae0a06bf0add9ef17c04636
elerac <maenosuke079@gmail.com>
2020-07-16 23:43:58 +0900

    fix : aries-python -> aries_python

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ git clone https://github.com/elerac/structured_light.git
 git clone https://github.com/elerac/fullscreen.git
 git clone https://github.com/elerac/PolarizationImaging.git
 git clone https://github.com/elerac/AutomaticPolarizer.git
-git clone https://github.com/2-propanol/aries-python.git
+git clone https://github.com/2-propanol/aries_python.git
