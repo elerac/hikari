5170e77af14557e821b43eacb2ed34e348cac10a
elerac <maenosuke079@gmail.com>
2020-07-16 23:11:16 +0900

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/elerac/EasyPySpin.git
+git clone https://github.com/elerac/structured_light.git
+git clone https://github.com/elerac/fullscreen.git
+git clone https://github.com/elerac/PolarizationImaging.git
+git clone https://github.com/elerac/AutomaticPolarizer.git
+git clone https://github.com/2-propanol/aries-python.git
