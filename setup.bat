3fa02d458c33f769f7243ed8898a6efc87cf53bb
elerac <maenosuke079@gmail.com>
2020-09-15 15:43:24 +0900

    Delete setup.bat

--- setup.bat
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/elerac/EasyPySpin.git
-git clone https://github.com/elerac/structured_light.git
-git clone https://github.com/elerac/fullscreen.git
-git clone https://github.com/elerac/PolarizationImaging.git
-git clone https://github.com/elerac/AutomaticPolarizer.git
-git clone https://github.com/2-propanol/aries_python.git
