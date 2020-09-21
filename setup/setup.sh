e8c95c4df5f1f0d2090cbf3be21fc4b019ddf48d
elerac <maenosuke079@gmail.com>
2020-09-21 18:01:40 +0900

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -2,4 +2,5 @@ pip3 install git+https://github.com/elerac/EasyPySpin
 pip3 install git+https://github.com/elerac/structuredlight
 pip3 install git+https://github.com/elerac/polanalyser
 pip3 install git+https://github.com/elerac/autopolarizer
+pip3 install git+https://github.com/elerac/fullscreen
 pip3 install git+https://github.com/2-propanol/aries_python
