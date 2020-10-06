cce13a2d96a2e97fffd00cb8d53e3093d7d55f7b
elerac <maenosuke079@gmail.com>
2020-10-07 01:22:43 +0900

    Update procam.py

--- procam.py
+++ procam.py
@@ -5,49 +5,73 @@
 # 外部モジュール
 import cv2
 import numpy as np
+import os
 # 自作ソフトウェア関連
 import structuredlight as sl
+import polanalyser as pa
 # 自作ハードウェア関連
 import PySpin
 import EasyPySpin
 from fullscreen import FullScreen
 
 def main():
+    # 出力するフォルダ名
+    dir_name = "mac_hf5x5"
+    os.makedirs(dir_name, exist_ok=True)
+
     # カメラの設定
-    cap = EasyPySpin.VideoCapture(0)
+    cap = EasyPySpin.VideoCaptureEX(0)
     #cap.set(cv2.CAP_PROP_GAMMA, 1.0)
-    cap.set(cv2.CAP_PROP_EXPOSURE, 3000)
+    cap.set(cv2.CAP_PROP_EXPOSURE, 10000)
     cap.set(cv2.CAP_PROP_GAIN, 0.0)
-    cap.set(cv2.CAP_PROP_FPS, 30)
+    cap.average_num = 4 # 平均化させる枚数，ノイズ耐性を上げる
+    t_min = 100
+    t_max = 200000
+    t_min = 1000
+    t_max = 40000
+    t_ref = 10000
+    num = None
    
     # プロジェクタの設定
     projector = FullScreen(1)
     width_prj  = projector.width
     height_prj = projector.height
-    img_black = np.full((width_prj, height_prj), 0, dtype=np.uint8)
-    projector.imshow(img_black)
+    projector.imshow(0)
 
     # 構造化光の設定
-    stlight = sl.Checker(sqsize=10, step=2)
+    stlight = sl.Checker(sqsize=5, step=1)
     imlist_pattern = stlight.generate((width_prj, height_prj))
     num = len(imlist_pattern)
     
-    # 環境光を除去する
-    cv2.waitKey(200)
-    ret, frame_black = cap.read()
+    # 漏れ光を除去用の画像を撮影する
+    cv2.waitKey(300)
+    #ret, frame_black = cap.read()
+    ret, frame_black = cap.readHDR(t_min, t_max, num, t_ref)
+    frame_black = pa.cvtStokesToIntensity(pa.demosaicing(frame_black))
 
     imlist_captured = []
     for i, pattern in enumerate(imlist_pattern):
         print("{}/{}".format(i+1, num))
         projector.imshow(pattern)
-        cv2.waitKey(200)
+        cv2.waitKey(300)
 
-        ret, frame = cap.read()
+        #ret, frame = cap.read()
+        ret, frame = cap.readHDR(t_min, t_max, num, t_ref)
+        frame = pa.cvtStokesToIntensity(pa.demosaicing(frame))
+        # 漏れ光を除去
+        dtype = frame.dtype
+        frame = np.abs(frame.astype(np.float64)-frame_black.astype(np.float64)).astype(dtype)
+        
+        name = f"{dir_name}/{dir_name}_{i+1}.exr"
+        cv2.imwrite(name, frame.astype(np.float32))
+
         imlist_captured.append(frame)
 
     img_direct, img_global = stlight.decode(imlist_captured)
-    cv2.imwrite("direct.png", img_direct.astype(np.uint8))
-    cv2.imwrite("global.png", img_global.astype(np.uint8))
+    #cv2.imwrite(f"{dir_name}/{dir_name}_direct.png", img_direct.astype(np.uint8))
+    #cv2.imwrite(f"{dir_name}/{dir_name}_global.png", img_global.astype(np.uint8))
+    cv2.imwrite(f"{dir_name}/{dir_name}_direct.exr", img_direct.astype(np.float32))
+    cv2.imwrite(f"{dir_name}/{dir_name}_global.exr", img_global.astype(np.float32))
 
     cap.release()
     projector.destroyWindow()
