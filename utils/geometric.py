270f872c4d255c75fc2aa8ac04e22cd53720ccc6
elerac <maenosuke079@gmail.com>
2020-10-07 12:07:42 +0900

    Create geometric.py

---
+++ utils/geometric.py
@@ -0,0 +1,138 @@
+"""
+Geometric calibration
+"""
+import numpy as np
+
+def cvtHeterogeneousToHomogeneous(points_hetero: np.ndarray) -> np.ndarray:
+    """
+    Convert Heterogeneous coordinates to Homogeneous coordinates system
+
+    Parameters
+    ----------
+    points_hetero : np.ndarray, (N, 3) or (N, 2)
+        Points in the Heterogeneous coordinate system
+    Returns
+    -------
+    points_homoge : np.ndarray, (N, 4) or (N, 3)
+        Points in the Homogeneous coordinate system
+    """
+    N, dim = points_hetero.shape
+    points_homoge = np.ones((N, dim+1), dtype=points_hetero.dtype)
+    points_homoge[:,:-1] = points_hetero
+    return points_homoge
+
+def calibrateCamera(objpoints: np.ndarray, imgpoints: np.ndarray) -> np.ndarray:
+    """
+    Calibrate camera
+
+    Parameters
+    ----------
+    objpoints : np.ndarray, (N, 3)
+        3D world point
+    imgpoints : np.ndarray, (N, 2)
+        2D image point
+    
+    Returns
+    -------
+    camera_matrix : np.ndarray, (3, 4)
+        camera matrix
+        [[c11, c12, c13, c14],
+         [c21, c22, c23, c24],
+         [c32, c32, c33, c34]]
+    """
+    # Check input array size
+    obj_N, obj_dim = objpoints.shape
+    img_N, img_dim = imgpoints.shape
+    assert obj_dim==3,   f"'objpoints' dimention must be 3: {obj_dim}"
+    assert img_dim==2,   f"'imgpoints' dimention must be 2: {img_dim}"
+    assert obj_N==img_N, f"Arrays must be the same size. objpoints:{obj_N}, img_points:{img_N}"
+    
+    N = obj_N
+
+    # Build matrix
+    A = np.zeros((N * 2, 11))
+    b = np.zeros((N * 2))
+    c34 = 1.0
+    for i in range(N):
+        X, Y, Z = objpoints[i]
+        x, y    = imgpoints[i]
+
+        A[2 * i]     = [X, Y, Z, 1, 0, 0, 0, 0, -X * x, -Y * x, -Z * x]
+        A[2 * i + 1] = [0, 0, 0, 0, X, Y, Z, 1, -X * y, -Y * y, -Z * y]
+        b[2 * i]     = c34 * x
+        b[2 * i + 1] = c34 * y
+
+    # Solve Ax=b
+    A_pinv = np.linalg.pinv(A)
+    x = A_pinv @ b # [c11, c12, c13, c14, c21, c22, c23, c24, c31, c32, c33]
+
+    camera_matrix = np.append(x, c34).reshape((3, 4))
+    return camera_matrix
+
+def calibrateCamera1D(objpoints: np.ndarray, imgpoints: np.ndarray) -> np.ndarray:
+    """
+    Calibrate camera 1D
+
+    Parameters
+    ----------
+    objpoints : np.ndarray, (N, 3)
+        3D world point
+    imgpoints : np.ndarray, (N,)
+        1D image point
+    
+    Returns
+    -------
+    camera_matrix_1d : np.ndarray, (2, 4)
+        camera matrix 1D
+        [[c11, c12, c13, c14],
+         [c32, c32, c33, c34]]
+    """
+    # Check input array size
+    obj_N, obj_dim = objpoints.shape
+    img_N = imgpoints.shape[0]
+    assert obj_dim==3,   f"'objpoints' dimention must be 3: {obj_dim}"
+    assert obj_N==img_N, f"Arrays must be the same size. objpoints:{obj_N}, img_points:{img_N}"
+    
+    N = obj_N
+
+    # Build matrix
+    A = np.zeros((N, 7))
+    b = np.zeros((N))
+    c34 = 1.0
+    for i in range(N):
+        X, Y, Z = objpoints[i]
+        x       = imgpoints[i]
+
+        A[i] = [X, Y, Z, 1, -X * x, -Y * x, -Z * x]
+        b[i] = c34 * x
+
+    # Solve Ax=b
+    A_pinv = np.linalg.pinv(A)
+    x = A_pinv @ b # [c11, c12, c13, c14, c31, c32, c33]
+
+    camera_matrix_1d = np.append(x, c34).reshape((2, 4))
+    return camera_matrix_1d
+
+def main():
+    N = 20
+    objpoints = np.random.rand(N, 3) # (N, 3)
+    
+    camera_matrix = np.random.rand(3, 4) # (3, 4)
+    camera_matrix[-1, -1] = 1 # set c34=1
+    print("Camera matrix GT:\n", camera_matrix)
+    
+    X = cvtHeterogeneousToHomogeneous(objpoints)
+    noise = np.random.normal(0, 0.5, (N, 3)) * 0.001
+    noise = 0
+    x = (camera_matrix @ X.T).T + noise
+   
+    img_points = x[:,:2] / np.expand_dims(x[:,2], 1) # (N, 2)
+    
+    camera_matrix = calibrateCamera(objpoints, img_points)
+    print("Camera matrix Estimate:\n", camera_matrix)
+
+    camera_matrix_1d = calibrateCamera1D(objpoints, img_points[:,0])
+    print("Camera matrix 1D Estimate:\n", camera_matrix_1d)
+
+if __name__=="__main__":
+    main()
